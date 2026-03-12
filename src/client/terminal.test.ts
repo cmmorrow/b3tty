281ea18c9721b79157502d554d2989181955dde0
Chris Morrow <cmmorrow@gmail.com>
2026-03-11 23:25:01 -0500

    Convert Javascript to Typescript * Moved terminal.js to terminal.ts * Added types.ts for client types

--- src/client/terminal.test.ts
+++ src/client/terminal.test.ts
@@ -10,7 +10,7 @@ import {
     handleSocketClose,
     sendResizeMessage,
     initTerm,
-} from "./terminal.mjs";
+} from "./terminal.ts";
 
 // ---------------------------------------------------------------------------
 // Shared mock factories
